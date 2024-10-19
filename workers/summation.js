5f7637d53c5fb8a5c9b40295c0f6905025236f62
nasifayc <nasifayc11@gmail.com>
2024-10-19 14:44:38 +0300

    worker-threads

--- workers/summation.js
+++ workers/summation.js
@@ -1,5 +1,8 @@
 // worker threads are usefull for handling CPU intensive tasks and for non-blockink code
 
+// Using worker threads allows the main thread to remain responsive and offload CPU-intensive tasks to separate threads.
+// This prevents blocking the event loop and improves performance for CPU-bound tasks.
+
 const { workerData, parentPort } = require("worker_threads");
 
 const n = workerData.n;
