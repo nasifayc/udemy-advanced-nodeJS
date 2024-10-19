ffe349b3362bcddbfce7d99f27f0ba988d71652e
nasifayc <nasifayc11@gmail.com>
2024-10-19 14:41:55 +0300

    starter-code

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const http = require("http");
 const url = require("url");
 const querystring = require("querystring");
+const { Worker } = require("worker_threads");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
+  res.write("Your request is being processed...\n");
+  console.log("Your request is being processed...\n");
   const query = url.parse(req.url).query;
   const n = Number(querystring.parse(query)["n"]);
-  let sum = 0;
-  for (let i = 0; i < n; i++) {
-    sum += i;
-  }
+  let sum = await findSum(n);
+
   console.log(sum);
-  res.end(String(sum));
+
+  res.end(`Sum = ${sum}`);
 });
+
+function findSum(n) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker("./workers/summation.js", {
+      workerData: { n },
+    });
+
+    worker.on("message", resolve);
+    worker.on("error", reject);
+  });
+}
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ workers/summation.js
@@ -0,0 +1,11 @@
+// worker threads are usefull for handling CPU intensive tasks and for non-blockink code
+
+const { workerData, parentPort } = require("worker_threads");
+
+const n = workerData.n;
+let sum = 0;
+for (let i = 0; i <= n; i++) {
+  sum = sum + i;
+}
+
+parentPort.postMessage(sum);
